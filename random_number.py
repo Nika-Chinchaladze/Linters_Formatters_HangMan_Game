c8335554a14fd48e11cc84f1bbb363bb86efbfa0
Nika-Chinchaladze <nika2019chinchaladze@gmail.com>
2023-05-22 16:31:22 +0400

    first pre-commit hook

--- random_number.py
+++ random_number.py
@@ -6,6 +6,7 @@ from random import randint
 
 
 def generate_random_number(num1: int, num2: int):
+
     """returns random number based on lowest and highest boundaries."""
     random_number = randint(num1, num2) if num1 < num2 else randint(num2, num1)
     return random_number
